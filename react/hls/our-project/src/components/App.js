3c8edd8edbda8e6ba2228213f477baab7634262f
brachazavcharov <82827701+brachazavcharov@users.noreply.github.com>
2021-05-13 19:58:22 +0300

    semantic-ui-commit

--- react/hls/our-project/src/components/App.js
+++ react/hls/our-project/src/components/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Icon, Button } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button.Group>
+        <Button>Cancel</Button>
+        <Button.Or />
+        <Button positive>Save</Button>
+      </Button.Group>
     </div>
   );
 }
